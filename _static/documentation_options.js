8da4a04a012f60fe9447064dcbfef761d4b54a4e
Val Hendrix <vchendrix@lbl.gov>
2019-06-19 13:53:05 -0700

    Changed inheritance diagrams

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2.0.0-rc1-2-g2348a06',
+    VERSION: 'v2.0.0-rc1-3-g532905e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
